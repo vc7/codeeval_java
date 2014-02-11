376b02cbf9e94a975735e47e8850dd5cc88f3ac6
Vincent <vince78718@gmail.com>
2014-02-11 23:24:27 +0800

    First Commit

---
+++ q23/src/q23/Main.java
@@ -0,0 +1,16 @@
+package q23;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <= 12; i++)
+		{
+			for(int j = 1; j <= 12; j++)
+			{
+				System.out.printf("%4s", j*i);
+			}
+			System.out.println();
+		}
+	}
+}
