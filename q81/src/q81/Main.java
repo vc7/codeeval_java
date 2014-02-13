6495586579842492cba8791a98679db9188638a4
Vincent <vince78718@gmail.com>
2014-02-13 23:49:41 +0800

    Commit Misc ;

--- q81/src/q81/Main.java
+++ q81/src/q81/Main.java
@@ -1,10 +1,42 @@
 package q81;
 
-public class Main {
+import java.util.*;
 
+public class Main {
+	
+	static int flag = 0;
+	static int chosen = 4;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String[] stringArray = new String[]{"A","B","C","D","E","F"};
+		
+		fix(stringArray, 0, 0);
+	}
+	
+	private static void fix(String[] array, int startIndex, int flag)
+	{
+		for(int i = flag; i <= array.length - chosen; i++)
+		{
+			choose(array, i);
+			System.out.println();
+		}
+	}
+	
+	private static void choose(String[] array, int currentIndex)
+	{
+		if(flag < chosen && currentIndex < array.length )
+		{
+			flag ++;
+			System.out.print("{"+array[currentIndex]+"},");
+			choose(array, currentIndex+1);
+		}
+		else
+		{
+			flag = 0;
+			System.out.println();
+			
+		}
 	}
 
 }
