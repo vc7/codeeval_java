86fe1225a194c36744c948771d5ce801e4e390f3
Vincent <vince78718@gmail.com>
2014-02-15 00:48:22 +0800

    Complete the answer and pass the question.

--- q99/src/q99/Main.java
+++ q99/src/q99/Main.java
@@ -4,22 +4,27 @@ import java.util.*;
 import java.util.regex.*;
 
 public class Main {
-	public static void main(String[] args)
+    public static void main(String[] args)
 	{
-		//		try {
-		//	        File file = new File(args[0]);
-		//	        Scanner sc = new Scanner(file);
-		//	        
-		//	    } catch (IOException e) {
-		//	        System.out.println("File Read Error: " + e.getMessage());
-		//	    }
+		try {
+			File file = new File(args[0]);
+			Scanner sc = new Scanner(file);
+			
+			String line;
+			
+            while (sc.hasNextLine() && (line = sc.nextLine()) != null) {
 
-		String text = "(25, 4) (1, -6)";
-		String regex = "[^\\(,\\) \\(,\\)]+";
-		
-		LinkedList<String> resultList = find(regex, text);
-		
-		System.out.println(calculateDistance(resultList));
+        		String regex = "[^\\(,\\) \\(,\\)]+";
+        		
+        		LinkedList<String> resultList = find(regex, line);
+        		
+        		System.out.printf("%d\n",calculateDistance(resultList).intValue());
+            }
+            sc.close();
+
+		} catch (IOException e) {
+			System.out.printf("File Read Error: " + e.getMessage());
+		}
 	}
 	
 	private static LinkedList<String> find(String regex, String content)
@@ -46,7 +51,7 @@ public class Main {
 		return list;
 	}
 	
-	private static double calculateDistance(LinkedList<String> list)
+	private static Double calculateDistance(LinkedList<String> list)
 	{
 		return Math.sqrt(Math.pow((Integer.parseInt(list.get(2))) - (Integer.parseInt(list.get(0))),2) + Math.pow((Integer.parseInt(list.get(3))) - (Integer.parseInt(list.get(1))),2)); 
 	}
