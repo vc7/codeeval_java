c6b551d1cd18e38913cef34a04f2efced9bb1d35
Vincent <vince78718@gmail.com>
2014-02-18 22:52:26 +0800

    Adding faster algorithm, start from 1000, break when find the first one

---
+++ q3/src/q3/Main_2.java
@@ -0,0 +1,51 @@
+package q3;
+
+public class Main_2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(getLastPalindromePrime());
+		
+	}
+	
+	private static int getLastPalindromePrime()
+	{
+		int i = 1000;
+		int target = 0;
+
+		while(i >= 0 || target != 0)
+		{
+			if(checkPrime(i) && checkPalindrome(i))
+			{
+				target = i;
+				break;
+			}
+			
+			i--;
+		}
+		
+		return target;
+	}
+	
+	private static boolean checkPalindrome(int number)
+	{
+		if (number == 11 || (number / 100 >= 1 && number / 100 == number % 10))
+		{
+			return true;
+		}
+
+		return false;
+	}
+	
+	private static boolean checkPrime(int number)
+	{
+		if (number <= 1) return false;
+		
+		for (int i = 2; i*i <= number; i ++)
+		{
+			if (number % i == 0) return false;
+		}
+		
+		return true;
+	}
+}
